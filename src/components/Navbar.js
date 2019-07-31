f5a41415c32bad54c514fd2afadd0cae56b80e87
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2019-07-31 12:26:47 -0500

    Sanity Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
-
+import logo from '../img/logo-w.png';
 
 export const Navbar = props => {
 
     return(
+        <div className="logo-wrap">
+        <img className="logo" src={logo}/>
         <h1 className="title">Anywhere Fitness</h1>
+        </div>
     )
 }
